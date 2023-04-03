5fa3d223b66c9e96aeaf505663d8fd3f87559cd2
rodzg <rodrigomr1993@gmail.com>
2023-04-03 19:34:28 -0300

    alteracao

--- Java/Main.java
+++ Java/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+ public class Main {
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 System.out.println("Digite o número da função que deseja executar: ");
@@ -14,75 +14,36 @@ int opcao = sc.nextInt();
 
     switch (opcao) {
         case 1:
-            System.out.print("Digite a quantidade de números que você deseja inserir: ");
-            int quantidadeNumeros = sc.nextInt();
-            int numerosInteiros = 0;
-            for (int i = 0; i < quantidadeNumeros; i++) {
-                System.out.print("Digite o número " + (i + 1) + ": ");
-                double numero = sc.nextDouble();
-                if (numero == (int) numero) {
-                    numerosInteiros++;
-                }
-            }
-            System.out.println("A quantidade de números inteiros é: " + numerosInteiros);
+            
+            Contagem item1 = new Contagem();
+            item1.contar();
             break;
 
         case 2:
-            System.out.print("Digite o número de termos que você deseja ver na série Fibonacci: ");
-            int n = sc.nextInt();
-            int primeiroTermo = 0;
-            int segundoTermo = 1;
-            System.out.println("Série Fibonacci de " + n + " termos:");
-            for (int i = 1; i <= n; i++) {
-                System.out.print(primeiroTermo + " ");
-                int proximoTermo = primeiroTermo + segundoTermo;
-                primeiroTermo = segundoTermo;
-                segundoTermo = proximoTermo;
-            }
+            Fibonacci item2 = new Fibonacci();
+            item2.fibo();
             break;
 
         case 3:
-            System.out.print("Digite o primeiro número: ");
-            int num1 = sc.nextInt();
-            System.out.print("Digite o segundo número: ");
-            int num2 = sc.nextInt();
-            int mdc = encontrarMDC(num1, num2);
-            System.out.println("O maior divisor comum entre " + num1 + " e " + num2 + " é: " + mdc);
+            MDC item3 = new MDC();
+            item3.mdc();
             break;
 
         case 4:
-            System.out.print("Digite o tamanho do vetor: ");
-            int tamanhoVetor = sc.nextInt();
-            int[] vetor = new int[tamanhoVetor];
-            for (int i = 0; i < tamanhoVetor; i++) {
-                System.out.print("Digite o número " + (i + 1) + ": ");
-                vetor[i] = sc.nextInt();
-            }
-            vetor = ordenarCrescente(vetor);
-            System.out.print("Vetor em ordem crescente: ");
-            for (int i = 0; i < tamanhoVetor; i++) {
-                System.out.print(vetor[i] + " ");
-            }
-            System.out.println();
+            OrdenarCrescente item4 = new OrdenarCrescente();
+            item4.ordem();
             break;
 
         case 5:
-            System.out.print("Digite um número: ");
-            int num = sc.nextInt();
-            boolean ehPrimo = verificarPrimo(num);
-            if (ehPrimo) {
-                System.out.println(num + " é um número primo");
-            } else {
-                System.out.println(num + " não é um número primo");
-            }
+            Primo item5 =  new Primo();
+            item5.primo();
             break;
 
         case 6:
-            System.out.print("Digite o tamanho do vetor: ");
-            int tamVetor = sc.nextInt();
-            int[] vetorSomatorio = new int[tamVetor];
-            for (int x=0; x < tamVetor; x++){
-                vetorSomatorio[x] = sc.nextInt();
-                int soma =0
-                soma = soma + vetorSomatorio[x];
-            }System.out.println(soma);
+            Somatorio item6 = new Somatorio();
+            item6.soma();
+           break;
+ }
+ }
+ }
+
